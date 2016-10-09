b01ba758f961b6a3d83bfe16d3678354055d3042
Soklay <soklay.chea@hotmail.com>
2016-10-09 16:07:16 +1100

    added a method to check if player is hit

--- method.cs
+++ method.cs
@@ -1,21 +1,5 @@
-﻿public bool isHit(int x, int y)
-
+// method returns true or false if player has been hit
+public bool isHit(int x, int y)
 {
-
-	if (player.position(x, y))
-
-	{
-
-		return true;
-
-	}
-
-		else 
-
-	{
-
-		return false;
-
-	}
-
+	// statement goes here
 }
