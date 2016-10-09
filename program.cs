9357a49b40a73eb38d650bec89d25d83bda8fad8
Soklay <soklay.chea@hotmail.com>
2016-10-09 18:01:06 +1100

    added age and education property

--- program.cs
+++ program.cs
@@ -1,16 +1,22 @@
-Public class Program 
+public class Program 
 {
 	private string _first;
 	private string _last;
+	private int _age;
+	private string _edu;
 
-	public Program(string fname, string lname)
+	public Program(string fname, string lname, int age, string edu)
 	{
 		_first = fname;
-		_last = lname
+		_last = lname;
+		_age = age;
+		_edu = edu;
 	}
 
 	public void Print()
 	{
 		Console.WriteLine("Hello this is " + _first + _last);
+		Console.WriteLine("I am " + _age + " years old");
+		Console.WriteLine("currently studying " + _edu);
 	}
 }
