dbfc69d8d35b88821c942dc8ed54b8fc20655bdd
Soklay <soklay.chea@hotmail.com>
2016-10-09 16:45:10 +1100

    added if player position is true takes dmg

--- method.cs
+++ method.cs
@@ -1,5 +1,8 @@
 // method returns true or false if player has been hit
-public bool isHit(int x, int y)
+public void isHit(int x, int y)
 {
-	// statement goes here
+	if (player.position(x, y))
+	{
+		player.life - dmg;
+	}
 }
