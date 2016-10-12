d33af3b6523117d2ece4334d88fd4187f7983c5e
Soklay <soklay.chea@hotmail.com>
2016-10-12 14:13:26 +1100

    asda

--- main.cs
+++ main.cs
@@ -1,33 +1,22 @@
-// Main Program with all method
+// Compile list of all classes
 
-namespace Game
-{
 
 public class Student 
 {
-	private string _first;
-	private string _last;
-	private int _age;
-	private string _edu;
-
-	public Student() : this ("Tim", "Tom", 21, "Swinburne")
-	{
-	}
-
-	public Student(string fname, string lname, int age, string edu)
-	{
-		_first = fname;
-		_last = lname;
-		_age = age;
-		_edu = edu;
-	}
-
-	public void Print()
-	{
-
-		Console.WriteLine("Hello this is {0} {1}", _first, _last);
-		Console.WriteLine("I am {0} years old", _age);
-		Console.WriteLine("currently studying {0}", _edu);	
-	}
+	// student class codes here
 }
+
+public class Tutor
+{
+	// methods related to tutor
+}
+
+public class School
+{
+	//
+}
+
+public class Unit
+{
+	//
 }
